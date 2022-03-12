a3145974ce6cc624b6c8db8a63c2b1ba2ec0cdb8
Taylor Everett <tayloreverett72@gmail.com>
2022-03-12 15:42:25 -0700

    Proper

--- WEEK-10/reciept.py
+++ WEEK-10/reciept.py
@@ -1,67 +1,37 @@
-
 import csv
-from datetime import datetime
-current_date_and_time = datetime.now()
-def read_products(filename, index):
-    products = {}
-    with open(filename, mode="rt") as product:
-        reader = csv.reader(product)
-        next(reader)
-        for row in reader:
-            key = row[index]
-            products[key] = [row[1], row[2]]
-    return products
-def process_request(filename, index, products):
-    requested=[]
-    x=0
-    with open(filename, mode="rt") as request:
-        reader = csv.reader(request)
-        next(reader)
-        """for line in reader:
-            requested = products[line[index]]
-            print(f"{requested[0]}: {line[1]} @ {requested[1]}")"""
-#HERE I TRY TO MAKE A LIST WITH ALL THE INFORMATION ITEM, QUNTIRY AND PRICE
-        for line in reader:
-            key = line[index]
-            req=[products[line[0]][0], line[1], products[line[0]][1]]
-            requested.append(req)
-            x=x+1
-    return requested   
-def main():
-    try:
-        index =0
-        detal = 1
-        value = 2
-        products = read_products("WEEK-10/products.csv", index)
-        #for line in products:
-            #print(f"{line} {products[line]}")
-        index=0
-        quantity=1
-        requested =process_request("WEEK-10/request.csv", index, products)
-        print("Inkom Emporium\n")
-        items= ''
-        subtotal=0
-        #HERE STARTS ALL THE MAGIC (OUTPUT)
-        for line in requested:
-            print(f"{line[0]}: {line[1]} @ {line[2]}")
-            items += int(line[1])
-            subtotal += float(line[2])*int(line[1])
-        tax = float(subtotal*0.06)
-        total = subtotal + tax
-        print(f"\nNumber of items: {items}")
-        print(f"Subtotal: {subtotal}\nSales Tax: {tax:.2f}")
-        print(f"Total: {total:.2f}\n")
-        print(f"Thank you for your shoppong at the Inkom Emporium\n{current_date_and_time:%A %I:%M %p}")
-        #TEST OF POSIBLE PROBLEMS
-    except KeyError as key_err:
-        print(type(key_err).__name__, key_err)
-    except FileNotFoundError as file_not_found_err:
-        print(file_not_found_err)
-
-
-
-    except PermissionError as perm_err:
-        print(f"Error: cannot read from products.csv because you don't have permission.")
-if __name__ == "__main__":
-    main()
-
+import datetime
+# Product fetch and setup
+products = {}
+totalItems = 0
+subtotal = 0.0
+total = 0.0
+print("Inkom Emporium")
+print()
+with open("products.csv", "rt") as infile:
+    reader = csv.reader(infile)
+    next(reader)
+    for row in reader:
+        prodNum = row[0]
+        prodName = row[1]
+        prodPrice = row[2]
+        dictionaryUpdate = {prodNum : (prodName, prodPrice)}
+        products.update(dictionaryUpdate)
+# Request fetch
+with open("request.csv", "rt") as infile:
+    reader = csv.reader(infile)
+    next(reader)
+    for row in reader:
+        prodNum = row[0]
+        prodQuantity = row[1]
+        split = products[prodNum]
+        totalItems += int(prodQuantity)
+        subtotal += float(split[1]) * float(prodQuantity)
+        print(f"{split[0]}: {prodQuantity} @ {split[1]}")
+print()
+print(f"Number of Items: {totalItems}")
+print(f"Subtotal: {subtotal}")
+print(f"Sales Tax: {round(subtotal * .06, 2)}")
+print(f"Total: {round(subtotal * 1.06, 2)}")
+print()
+print("Thank you for shopping at the Inkom Emporium.")
+print(datetime.datetime.now().strftime("%a %b   %d %I:%M:%S %Y"))
